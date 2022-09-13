7ee15844dbc4854da4c897734e7bf2617efbee40
Nia K <nsjkappen@gmail.com>
2022-09-12 18:43:24 -0700

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+  return (
+  <header className="header">
+      <nav className="nav container"></nav>
+      <a href="index.html" className="nav__logo">Kappen</a>
+      <div className="nav__menu">
+
+          <ul className="nav__list grid">
+              <li className="nav__item"></li>
+
+          </ul>
+
+
+
+
+
+      </div>
+      
+
+  </header>
+  )
+}
+
+export default Header
